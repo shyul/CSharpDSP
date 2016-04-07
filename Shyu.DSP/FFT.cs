d9bd4ab331ce9ad3465f44d1d593e6bcb19af815
Shyu Lee <shyu.lee@gmail.com>
2016-04-06 20:20:06 -0700

    FFT: Start

---
+++ Shyu.DSP/FFT.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shyu.Core;
+
+namespace Shyu.DSP
+{
+    public static class FFT
+    {
+    }
+}
